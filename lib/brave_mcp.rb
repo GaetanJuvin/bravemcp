42fb8b329f1ced7fc2e024420ae76fbf2d492a43
Gaetan JUVIN <gaetanjuvin@gmail.com>
2026-01-31 15:35:48 -0800

    feat: add startup connection check and polish

--- lib/brave_mcp.rb
+++ lib/brave_mcp.rb
@@ -4,6 +4,10 @@ require "ferrum"
 
 module BraveMcp
   VERSION = "0.1.0"
+
+  module Tools
+    # Tools will be auto-loaded
+  end
 end
 
 require_relative "brave_mcp/browser"
--- lib/brave_mcp/server.rb
+++ lib/brave_mcp/server.rb
@@ -4,16 +4,23 @@ module BraveMcp
     def self.run
       server = FastMcp::Server.new(name: "brave-mcp", version: VERSION)
 
-      # Tools will be registered here
       register_tools(server)
+
+      # Attempt connection on startup to fail fast with helpful message
+      begin
+        Browser.instance
+        $stderr.puts "BraveMCP connected to Brave browser"
+      rescue Browser::ConnectionError => e
+        $stderr.puts "Warning: #{e.message}"
+        $stderr.puts "Tools will attempt to connect on first use."
+      end
 
       server.start
     end
 
     def self.register_tools(server)
-      Dir[File.join(__dir__, "tools", "*.rb")].each { |f| require f }
+      Dir[File.join(__dir__, "tools", "*.rb")].sort.each { |f| require f }
 
-      # Register all tool classes
       Tools.constants.each do |const|
         tool_class = Tools.const_get(const)
         server.register_tool(tool_class) if tool_class < FastMcp::Tool
